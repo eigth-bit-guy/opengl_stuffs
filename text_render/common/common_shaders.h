64b21e73e7507a1b68196d0cff2022b1d8e5f505
Samuel Henrique <henriquedossantosq8d@gmail.com>
2024-05-05 18:35:14 -0300

    feat: common directory

---
+++ text_render/common/common_shaders.h
@@ -0,0 +1,7 @@
+#ifndef _COMMON_SHADERS_H_
+#define _COMMON_SHADERS_H_
+
+GLint get_attrib(GLuint program, const char *name);
+GLint get_uniform(GLuint program, const char *name);
+
+#endif //_COMMON_SHADERS_H_
