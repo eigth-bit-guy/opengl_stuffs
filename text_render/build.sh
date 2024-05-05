f10ce2734e2125e04aa3feb0826866aadbe70552
Samuel Henrique <henriquedossantosq8d@gmail.com>
2024-05-05 18:34:46 -0300

    feat: build script

---
+++ text_render/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -xe
+
+CC="${CXX:-cc}"
+PKGS="sdl2 glew freetype2"
+CFLAGS="-Wall -Wextra -std=c11 -pedantic -ggdb"
+LIBS=-lm
+SRC="main.c common/common_shaders.c"
+
+if [ `uname` = "Darwin" ]; then
+    CFLAGS+=" -framework OpenGL"
+fi
+
+$CC $CFLAGS `pkg-config --cflags $PKGS` -o main $SRC $LIBS `pkg-config --libs $PKGS`
